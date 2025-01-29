9d73d7de09199c0ae1186cc01022e3b3126f13cc
KCkingcollin <kckingcollin99@gmail.com>
2025-01-29 02:21:26 -0500

    Update

--- main.go
+++ main.go
@@ -1,33 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
+	"strings"
 )
 
 func main() {
-    var inTE *walk.TextEdit
+	var inTE *walk.TextEdit
+	var mainWin *walk.MainWindow
 
-    MainWindow{
-        Title:   "Text Input Example",
-        MinSize: Size{Width: 100, Height: 75},
-        Layout:  VBox{},
-        Children: []Widget{
-            TextEdit{
-                AssignTo: &inTE,
-                OnKeyPress: func(key walk.Key) {
-                    if key == walk.KeyReturn {
-                        inputText := strings.TrimSpace(inTE.Text())
-                        if inputText != "" {
-                            fmt.Println(inputText)
-                            inTE.SetText("")
-                        }
-                    }
-                },
-            },
-        },
-    }.Run()
+	MainWindow{
+		AssignTo: &mainWin,
+		Title:    "TTS Input",
+		MinSize:  Size{Width: 250, Height: 120},
+		Layout:   VBox{},
+		Children: []Widget{
+			TextEdit{
+				AssignTo: &inTE,
+				MinSize:  Size{Width: 200, Height: 40},
+				MaxSize:  Size{Width: 200, Height: 40},
+				Font:     Font{PointSize: 10},
+				OnKeyPress: func(key walk.Key) {
+					if key == walk.KeyReturn {
+						sendText(inTE, mainWin)
+					}
+				},
+			},
+			PushButton{
+				Text: "Send",
+				MinSize: Size{Width: 80, Height: 30},
+				OnClicked: func() {
+					sendText(inTE, mainWin)
+				},
+			},
+		},
+	}.Run()
+}
+
+func sendText(inTE *walk.TextEdit, owner walk.Form) {
+	inputText := strings.TrimSpace(inTE.Text())
+	if inputText != "" {
+		walk.MsgBox(owner, "Input Received", inputText, walk.MsgBoxIconInformation)
+		inTE.SetText("")
+	}
 }
