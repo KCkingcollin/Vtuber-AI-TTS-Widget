c4ef32f16477cce5235af81a5f51a74f1c5716e3
KCkingcollin <kckingcollin99@gmail.com>
2025-01-30 20:24:16 -0500

    Update

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"os"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 	"github.com/KCkingcollin/Vtuber-AI-TTS-Widget/src"
+	"github.com/go-vgo/robotgo"
 	hook "github.com/robotn/gohook"
 )
 
@@ -30,6 +32,32 @@ func main() {
     entry := widget.NewEntry()
     entry.SetPlaceHolder("TTS Input...")
     
+    howMany, _ := robotgo.FindIds("main")
+    if len(howMany) > 1 {
+        log.Append("To many processes", verbose)
+        error := widget.NewLabel("Error: To many processes")
+        text := widget.NewLabel("Just throw in a whole banana (peal included)?")
+        yes := widget.NewButton("YES", func() {
+            for i := 0; i < len(howMany); i++ {
+                process, err := os.FindProcess(howMany[i])
+                if err != nil {
+                    log.Append(fmt.Sprintf("%e", err), verbose)
+                }
+                process.Kill()
+            }
+        })
+        no := widget.NewButton("no", func() {
+            os.Exit(0)
+        })
+        text.Alignment = fyne.TextAlignCenter
+        error.Alignment = fyne.TextAlignCenter
+        buttons := container.NewHBox(yes, no)
+        centeredButtons := container.NewCenter(buttons)
+        content := container.NewVBox(error, text, centeredButtons)
+        myWindow.SetContent(container.NewVBox(content))
+        myWindow.ShowAndRun()
+    }
+
     button := widget.NewButton(" Press to send (or just press enter) ", func() {
         sendText(entry.Text)
         entry.SetText("")
@@ -47,6 +75,7 @@ func main() {
     isHidden := false
 
 	log.Append("Press Ctrl+M to toggle window minimize state", verbose)
+
 
     go func() {
         hook.Register(hook.KeyDown, []string{"ctrl", "shift", "m"}, func(e hook.Event) {
